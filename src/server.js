25d8798f0093c4dcda5d4027e31f3a1664f32af3
Carlos Tribiec <carlosjosetribiec@hotmail.com>
2020-04-30 02:28:13 -0400

    Deploy configuration

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";;
 import router from './routes';
 import cors from 'cors';
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
