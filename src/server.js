5c364cdfe86f39f097908b259433030dd837d79f
Carlos Tribiec <carlosjosetribiec@hotmail.com>
2020-04-30 02:51:47 -0400

    Fix

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import express from "express";;
 import router from './routes';
 import cors from 'cors';
+import {config as envSet} from "dotenv";
+envSet();
 const app = express();
 const port = process.env.PORT || 8080;
-
 app.use(cors());
 app.use(express.json());
 app.use(router);
