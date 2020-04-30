7f4e3e87236cbabc0b899e965ea92fbaf3e79f8a
Carlos Tribiec <carlosjosetribiec@hotmail.com>
2020-04-30 00:57:31 -0400

    Heroku API agregado

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,8 @@
 import { MongoClient } from "mongodb";
 const url = "mongodb://localhost:27017";
-const dbName = "misCitasApp";
-const client = new MongoClient(url, { useUnifiedTopology: true });
+const uri = "mongodb://heroku_d20lv468:6u3fpt2anm350bn1i2a9v27l5k@ds255308.mlab.com:55308/heroku_d20lv468";
+const dbName = "heroku_d20lv468";
+const client = new MongoClient(uri, { useUnifiedTopology: true });
 client.connect();
 const db = client.db(dbName);
 console.log("Connected to DB");
--- src/server.js
+++ src/server.js
@@ -7,6 +7,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(router);
+app.get('/*', (req,res) => {
+    res.send("<h1>MisCitas API</h1>");
+});
 app.listen(port, () => {
     console.log(`Server started on port: ${port}`);
 });
