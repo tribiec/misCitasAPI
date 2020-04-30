460586695dd127127086a019d8ff12111530bc57
Carlos Tribiec <carlosjosetribiec@hotmail.com>
2020-04-30 02:40:20 -0400

    API Endpoint changed

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 const url = "mongodb://localhost:27017";
-const uri = "mongodb://heroku_d20lv468:6u3fpt2anm350bn1i2a9v27l5k@ds255308.mlab.com:55308/heroku_d20lv468";
-const dbName = "heroku_d20lv468";
+const uri = "mongodb://heroku_863t28vz:4ofivrqere41q0i8856uuodg57@ds129641.mlab.com:29641/heroku_863t28vz";
+const dbName = "heroku_863t28vz";
 const client = new MongoClient(uri, { useUnifiedTopology: true });
 client.connect();
 const db = client.db(dbName);
